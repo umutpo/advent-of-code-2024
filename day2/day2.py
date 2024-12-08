33e13d4909dec3f505748e461d1743f76c5cbd9e
TheUnfocused <33618431+umutpo@users.noreply.github.com>
2024-12-08 01:40:20 -0800

    Solved day2

--- day2/day2.py
+++ day2/day2.py
@@ -1,6 +1,17 @@
+def isIncreasing(level: list[int]) -> bool:
+    levelCount = len(level)
+    
+    positives = 0
+    for i in range(1, levelCount):
+        if level[i] - level[i - 1] > 0:
+            positives += 1
+    
+    return (levelCount // 2) < positives 
+    
+
 def partOne(level: list[int]) -> tuple[bool, int]:
     levelCount = len(level)
-    increasing = True if level[0] - level[levelCount - 1] < 0 else False
+    increasing = isIncreasing(level)
     
     for index in range(levelCount - 1):
         difference = level[index + 1] - level[index]
@@ -20,20 +31,15 @@ with open('./input.txt', 'r') as input_file:
     for i in range(levelsCount):
         level = levels[i]
         isSafe, lastIndex = partOne(level)
-
-        print("Trying {}".format(level))
         if isSafe:
-            print("\tSafe on first try")
             safeLevelCountPartOne += 1
             safeLevelCountPartTwo += 1
         else:
-            newLevel = level[:lastIndex] + level[lastIndex + 1:]
-            print("\tTrying again with {}".format(newLevel))
-            
-            isSafeSkipped = partOne(level[:lastIndex] + level[lastIndex + 1:])
-            if isSafeSkipped:
-                print("\tSafe on second try")
-                safeLevelCountPartTwo += 1
+            for k in range(len(level)):
+                isSafeSkipped, lastIndex = partOne(level[:k] + level[k + 1:])
+                if isSafeSkipped:
+                    safeLevelCountPartTwo += 1
+                    break
     
     print("Part 1: {}".format(safeLevelCountPartOne))
     print("Part 2: {}".format(safeLevelCountPartTwo))
